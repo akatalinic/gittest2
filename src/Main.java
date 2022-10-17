5f1ab1edc40790653beb10fe3f8e7e7e9a3c3872
Ante Katalinić <ante.katalinic1@gmail.com>
2022-10-17 14:37:34 +0200

    My Second commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         Person firstPerson = new Person("Pedro Remoaldo", "Portugal");
+        System.out.println(firstPerson.getName());
 
     }
 }
